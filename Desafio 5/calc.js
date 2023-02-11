7cd8d80f7189bf1b445fa6365a58f095ef0d4509
gabriel <gabriell.passos7@gmail.com>
2023-02-10 22:48:02 -0300

    Desafios Rocket Seat

---
+++ Desafio 5/calc.js
@@ -0,0 +1,30 @@
+
+function clean() {
+    document.querySelector('#final').innerHTML = '';
+    document.querySelector('#calc').innerHTML = '';
+}
+
+function backSpace () {
+    var operand = document.querySelector('#calc').innerHTML;
+    document.querySelector('#calc').innerHTML = operand.substring(0, operand.length -1)
+}  
+
+function insert (valor) {
+    document.querySelector('#calc').innerHTML += valor
+}
+
+function equals () {
+    var tela = document.querySelector('#calc').innerHTML;
+    
+    eval(tela)
+       
+    document.querySelector('#final').innerHTML = eval(tela).toFixed(2)
+
+}
+
+function percent () {
+    var porce = document.querySelector('#calc').innerHTML
+    document.querySelector('#calc').innerText = porce / 100;
+}
+ 
+
